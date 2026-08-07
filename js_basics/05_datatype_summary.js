7571412e586fade162b9c3df11e7bd5a8540d7da
Adamya Sundriyal <adamyasundriyal001@gmail.com>
2026-08-07 10:28:29 +0000

    stack and heap memory added

--- js_basics/05_datatype_summary.js
+++ js_basics/05_datatype_summary.js
@@ -3,29 +3,29 @@
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
 const score = 100
-console.log(typeof score)
+// console.log(typeof score)
 
 const scoreValue = 100.3
-console.log(typeof scoreValue)
+// console.log(typeof scoreValue)
 
 const isLoggedIn = false
-console.log(typeof isLoggedIn)
+// console.log(typeof isLoggedIn)
 
 const outsideTemp = null
-console.log(typeof outsideTemp)
+// console.log(typeof outsideTemp)
 
 let userEmail;
-console.log(typeof userEmail)
+// console.log(typeof userEmail)
 
 const id = Symbol('123')
-console.log(typeof id)
+// console.log(typeof id)
 const anotherId = Symbol('123')
-console.log(typeof anotherId)
+// console.log(typeof anotherId)
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const bigNumber = 3456543576654356754n
-console.log(typeof bigNumber)
+// console.log(typeof bigNumber)
 
 
 // Reference (Non primitive)
@@ -37,10 +37,34 @@ let myObj = {
     name: "hitesh",
     age: 22,
 }
-console.log(typeof heros)
+// console.log(typeof heros)
 
 const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof myFunction)
+// console.log(typeof myFunction)
+
+// *******************************************************************
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "AdamyaSundriyal"
+
+let anotherName = myYoutubeName
+anotherName = "AdamWarlock"
+
+// console.log(myYoutubeName)
+// console.log(anotherName)
+
+let userOne = {
+    userId: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "adamya@google.com"
+
+// console.log(userOne.email)
+// console.log(userTwo.email)
