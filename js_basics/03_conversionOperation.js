1d6d2394a067eb82a5e2df5cb2f39b7052d7baa7
Adamya Sundriyal <adamyasundriyal001@gmail.com>
2026-08-07 07:40:04 +0000

    conversion of datatypes

---
+++ js_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "adamya"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// convert into Number
+// "33" => 33
+// null => 0
+// "33abc" => NaN (Not an Number)
+// true=>1/false=>0
+// undefined => NaN
+
+let isLoggedIn = "adam"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// convert into Boolean
+// 1=>true/0=>false
+// ""=>false
+// "adam"=>true
+
+
+//convert into String
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
