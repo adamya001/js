d11184be676e5333f562ac624262b78f4edb954e
Adamya Sundriyal <adamyasundriyal001@gmail.com>
2026-08-09 10:00:49 +0000

    another change

---
+++ 01_js_basics/03_conversionOperation.js
@@ -0,0 +1,85 @@
+let score = "adamya"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// convert into Number
+// "33" => 33
+// null => 0
+// "33abc" => NaN (Not an Number)
+// true=>1/false=>0
+// undefined => NaN
+
+let isLoggedIn = "adam"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// convert into Boolean
+// 1=>true/0=>false
+// ""=>false
+// "adam"=>true
+
+
+//convert into String
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ************************* OPERATIONS *******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " adamya"
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log( (3+4) / 5 % 2)
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++; 
+++gameCounter;
+console.log(gameCounter)
+
+// If used postfix, with operator after operand (for example, x++),
+// the increment operator increments and returns the value before incrementing.
+/*
+let x = 3;
+const y = x++;
+x is 4; y is 3
+*/
+
+
+// If used prefix, with operator before operand (for example, ++x),
+// the increment operator increments and returns the value after incrementing.
+/*
+let x = 3;
+const y = ++x;
+x is 4; y is 4
+*/
+
---
+++ 01_js_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log( 2 < 1 )
+// console.log( 2 <= 1 )
+// console.log( 2 > 1 )
+// console.log( 2 == 1 )
+// console.log( 2!=1 )
+
+// console.log( "2" > 1)
+// console.log( "02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// Note:- Equality check == and comparisons >,>=,<,<= work differently.
+// Comparison convert null to a number, treating as 0
+// that's why line (10) gives false and (12) gives true.
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+
+// === ( strict equal to )
+
+console.log("2" == 2)
+console.log("2" === 2) // also check datatype
