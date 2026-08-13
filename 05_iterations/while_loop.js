47ec3bf87716beb3628dbb980645354c15580611
Adamya Sundriyal <adamyasundriyal001@gmail.com>
2026-08-13 08:33:07 +0000

    while loop

---
+++ 05_iterations/while_loop.js
@@ -0,0 +1,28 @@
+// while loop
+// while(condition){
+   //statements
+// }
+
+// let i=0
+// while(i<=10){
+//     console.log(`Value of ${i}`);
+//     i=i+2
+// }
+
+// let myArray = ["Spider Man","Iron Man","Thor"]
+// let arr = 0
+// while(arr<myArray.length){
+//     console.log(`Value is ${myArray[arr]}`)
+//     arr=arr+1
+// }
+
+// do{
+   //statements
+// }while(condition);
+
+let score = 11
+do{
+    console.log(`Score is ${score}`);
+    score=score+1;
+}while(score<10);
+
